12040de91471343333165b1f3e4ed9bdd02b15e4
KaushikDebdas <kaushikdebdas27@gmail.com>
2024-03-27 12:56:03 +0600

    How to use conftest.py file for setup and teardown for all py files and newlly add autouse=True

--- PytestBasicPractice/9_Creating_fixture_inside_conftest.py_file/conftest.py
+++ PytestBasicPractice/9_Creating_fixture_inside_conftest.py_file/conftest.py
@@ -3,11 +3,12 @@ In Pytest,
 - if we have multiple file in a folder and then we need to run same test again and again, then we need fixture
 - the fixture format can store in a separate file called conftest.py
 - this file use centrally
+- if we use autouse=True inside the pytest.fixture() then it will automatic set attribute in every other file
 '''
 import pytest
 
 
-@pytest.fixture()
+@pytest.fixture(autouse=True)
 def test_setup_and_teardown():
     print("Launch Browser")
     print("Open Application")
