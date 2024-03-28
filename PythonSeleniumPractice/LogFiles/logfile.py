1fa75e6f088e3c2674c08d26903c13f7b2db4f69
KaushikDebdas <kaushikdebdas27@gmail.com>
2024-03-28 14:56:00 +0600

    logfile updated

--- PythonSeleniumPractice/LogFiles/logfile.py
+++ PythonSeleniumPractice/LogFiles/logfile.py
@@ -1,8 +1,7 @@
 import logging
-from datetime import datetime
 
 LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
-LOG_FILE = f"D:/Study Videos/Python/pythonProject/SampleProject3_POM/tests/demofile44.log"
+LOG_FILE = f"demofile44.log"
 
 def log_details():
     logging.basicConfig(
@@ -12,12 +11,12 @@ def log_details():
     )
     return logging.getLogger()
 
-# logger = log_details()
+logger = log_details()
 
-# a =4
-# b =3
-# if a>b:
-#     print("Kaushik")
-#     logger.info("a is greater than b, hence Kaushik got printed in output")
-#
-# logger.info("Execution stop")
+a =4
+b =3
+if a>b:
+    print("Kaushik")
+    logger.info("a is greater than b, hence Kaushik got printed in output")
+
+logger.info("Execution stop")
