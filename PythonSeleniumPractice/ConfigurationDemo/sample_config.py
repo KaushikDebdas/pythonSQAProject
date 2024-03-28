9803073f96eca584f614a9640c491e5cee15c60c
KaushikDebdas <kaushikdebdas27@gmail.com>
2024-03-28 13:04:42 +0600

    function added here

--- PythonSeleniumPractice/ConfigurationDemo/sample_config.py
+++ PythonSeleniumPractice/ConfigurationDemo/sample_config.py
@@ -11,15 +11,19 @@ to get data use config.get("categoryName", "categoryInputName")
 '''
 from configparser import ConfigParser
 
-config = ConfigParser()
-config.read("config.ini")
+def get_config(category,key):
+    config = ConfigParser()
+    config.read("config.ini")
+    return config.get(category,key)
 
-url_retrieved = config.get("basic info" , "url")
+url_retrieved = get_config("basic info", "url")
 print(url_retrieved)
-browser_retrieved = config.get("basic info" , "browser")
+
+browser_retrieved = get_config("basic info", "browser")
 print(browser_retrieved)
 
-search_field_retrieved = config.get("locator search" , "search_field")
+search_field_retrieved = get_config("locator search" , "search_field")
 print(search_field_retrieved)
-search_button_retrieved = config.get("locator search", "search_button")
+
+search_button_retrieved = get_config("locator search" , "search_button")
 print(search_button_retrieved)
